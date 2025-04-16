55d18189d884646e93925c4123db451018a6c646
alexRicc2 <alex.65.santanna@gmail.com>
2025-04-16 00:47:08 -0300

    header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Container from "../container";
 import { Button } from "@/components/ui/button";
-import cn from 'clsx'
+import cn from "clsx";
 import {
   Sheet,
   SheetContent,
@@ -20,7 +20,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
-import { Menu } from "lucide-react";
+import { Menu, ChartArea, NotebookPen } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -90,37 +90,39 @@ function Header() {
         </Link>
 
         {/* Desktop Navigation */}
-        <NavigationMenu className="hidden md:flex md:!max-w-[600px] lg:!max-w-[700px] !w-full" orientation="rtl">
+        <NavigationMenu
+          className={`hidden md:flex 
+          `}
+          orientation="rtl"
+        >
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Free Tools</NavigationMenuTrigger>
-              <NavigationMenuContent orientation="ltr" dir="ltr">
-                <ul className="grid gap-3 p-6 md:w-[600px] lg:w-[700px] lg:grid-cols-[1.2fr_1fr_1fr]">
-                  <li className="row-span-2">
-                    <NavigationMenuLink asChild>
-
-                      <a
-                        className="flex h-full w-full select-none flex-col justify-center rounded-md bg-gradient-to-b from-muted/50 to-gray-100 p-6 no-underline outline-none focus:shadow-md"
-                        href="/free-tools"
-                      >
-                        <svg className="feather feather-tool" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" /></svg>
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          TSH FREE Tools
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          The SEO Hustler Tools, get better insights of today&apos;s keyword
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/seo-check" title="SEO Check On Page">
-                    Provide an url and get its SEO score
+              <NavigationMenuTrigger className="cursor-pointer">Free Tools</NavigationMenuTrigger>
+              <NavigationMenuContent orientation="ltr" dir="ltr" className="shadow-lg rounded-md">
+                <ul className="flex flex-col gap-3 p-2">
+                  <ListItem href="/seo-check" title="SEO Check On Page" free>
+                    <div className="flex items-center space-x-3">
+                      <svg width="20" height="20" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg" className="min-w-5 min-h-5 rounded-md">
+                        <path d="M35 17.5V12.25L26.25 3.5H10.5C9.57174 3.5 8.6815 3.86875 8.02513 4.52513C7.36875 5.1815 7 6.07174 7 7V35C7 35.9283 7.36875 36.8185 8.02513 37.4749C8.6815 38.1313 9.57174 38.5 10.5 38.5H17.5" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M24.5 3.5V10.5C24.5 11.4283 24.8687 12.3185 25.5251 12.9749C26.1815 13.6313 27.0717 14 28 14H35" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M26 29L28 31L32 27" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M29 37C33.4183 37 37 33.4183 37 29C37 24.5817 33.4183 21 29 21C24.5817 21 21 24.5817 21 29C21 33.4183 24.5817 37 29 37Z" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M39 39.0002L34.7 34.7002" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                      </svg>
+                      <span>Provide an URL and get its SEO score</span>
+                    </div>
                   </ListItem>
-                  <ListItem href="/advanced-keyword-analysis" title="Advanced Keyword Analysis">
-                    Provide a keyword and get its SEO insights
+                  <ListItem href="/advanced-keyword-analysis" title="Advanced Keyword Analysis" free>
+                    <div className="flex items-center space-x-3">
+                      <ChartArea width={20} height={20} strokeWidth={1.5} className="min-w-5 min-h-5 rounded-md" color="black" />
+                      <span>Provide a keyword and get its SEO insights</span>
+                    </div>
                   </ListItem>
-                  <ListItem href="/content-planning" title="Content Planning">
-                    Provide a keyword and we will give you a content plan
+                  <ListItem href="/content-planning" title="Content Planning" free>
+                    <div className="flex items-center space-x-3">
+                      <NotebookPen width={20} height={20} strokeWidth={1.5} className="min-w-5 min-h-5 rounded-md" color="black" />
+                      <span>Provide a keyword and we will give you a content plan</span>
+                    </div>
                   </ListItem>
                 </ul>
               </NavigationMenuContent>
@@ -145,8 +147,6 @@ function Header() {
                 <NavigationMenuLink>Blog</NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-
-
           </NavigationMenuList>
         </NavigationMenu>
 
@@ -170,50 +170,56 @@ function Header() {
             </SheetHeader>
             <div className="flex flex-col space-y-4 mt-4 px-4">
               <SheetClose asChild>
-
-                <Link href="/free-tools" className="text-sm font-medium">
+                <Link href="/free-tools" className="text-sm font-medium cursor-pointer">
                   Free Tools
                 </Link>
               </SheetClose>
               <SheetClose asChild>
-
+                <Link href="/seo-check" className="text-sm font-medium flex items-center space-x-1">
+                  <svg width="20" height="20" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M35 17.5V12.25L26.25 3.5H10.5C9.57174 3.5 8.6815 3.86875 8.02513 4.52513C7.36875 5.1815 7 6.07174 7 7V35C7 35.9283 7.36875 36.8185 8.02513 37.4749C8.6815 38.1313 9.57174 38.5 10.5 38.5H17.5" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M24.5 3.5V10.5C24.5 11.4283 24.8687 12.3185 25.5251 12.9749C26.1815 13.6313 27.0717 14 28 14H35" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M26 29L28 31L32 27" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M29 37C33.4183 37 37 33.4183 37 29C37 24.5817 33.4183 21 29 21C24.5817 21 21 24.5817 21 29C21 33.4183 24.5817 37 29 37Z" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M39 39.0002L34.7 34.7002" stroke="black" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" />
+                  </svg>
+                  <span>
+                    SEO Check On Page
+                  </span>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link
+                  href="/advanced-keyword-analysis"
+                  className="text-sm font-medium flex items-center space-x-1"
+                >
+                  <ChartArea width={20} height={20} strokeWidth={1.5} className=" rounded-md" />
+                  <span>Advanced Keyword Analysis</span>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/content-planning" className="text-sm font-medium flex items-center space-x-1">
+                  <NotebookPen width={20} height={20} strokeWidth={1.5} className=" rounded-md" />
+                  <span>Content Planning</span>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
                 <Link href="/seo-check" className="text-sm font-medium">
-                  SEO Check On Page
+
+                  <span>SEO Check On Page</span>
                 </Link>
               </SheetClose>
               <SheetClose asChild>
-
-                <Link href="/advanced-keyword-analysis" className="text-sm font-medium">
-                  Advanced Keyword Analysis
-                </Link>
-              </SheetClose>
-              <SheetClose asChild>
-
-                <Link href="/content-planning" className="text-sm font-medium">
-                  Content Planning
-                </Link>
-              </SheetClose>
-              <SheetClose asChild>
-
-                <Link href="/seo-check" className="text-sm font-medium">
-
-                  SEO Check On Page
-                </Link>
-              </SheetClose>
-              <SheetClose asChild>
-
                 <Link href="/blog" className="text-sm font-medium">
                   Blog
                 </Link>
               </SheetClose>
               <SheetClose asChild>
-
                 <Link href="/resources" className="text-sm font-medium">
                   Resources
                 </Link>
               </SheetClose>
               <SheetClose asChild>
-
                 <Link href="/about" className="text-sm font-medium">
                   About
                 </Link>
@@ -225,7 +231,10 @@ function Header() {
     </header>
   );
 }
-const ListItem = (({ className, title, children, href, free = false, ...props }, ref) => {
+const ListItem = (
+  { className, title, children, href, free = false, ...props },
+  ref
+) => {
   const pathname = usePathname();
   const [fullPath, setFullPath] = useState("");
 
@@ -240,7 +249,7 @@ const ListItem = (({ className, title, children, href, free = false, ...props },
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors bg-gray-100 hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors bg-gray-100/50 hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
           href={href}
@@ -249,22 +258,21 @@ const ListItem = (({ className, title, children, href, free = false, ...props },
           <div
             className={cn(
               "text-sm whitespace-nowrap font-medium leading-none",
-              { 'text-[#ffc000]': fullPath === href } // Compare full path (including hash) with href
+              { "text-[#ffc000]": fullPath === href } // Compare full path (including hash) with href
             )}
           >
-            {title} {free && <span className='text-green-400 font-semibold'>FREE</span>}
+            {title}{" "}
+            {free && <span className="ml-2 text-green-600 font-semibold">FREE</span>}
           </div>
-          <p className="line-clamp-2 text-xs leading-snug text-[#555]">
+          <div className="line-clamp-2 text-xs leading-snug text-[#555]">
             {children}
-          </p>
+          </div>
         </a>
       </NavigationMenuLink>
     </li>
   );
-});
+};
 
 ListItem.displayName = "ListItem";
-
-
 
 export default Header;
