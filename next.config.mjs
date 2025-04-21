21a2969e453964d36772b62d916840dc22a10480
alexRicc2 <alex.65.santanna@gmail.com>
2025-04-21 12:10:40 -0300

    testing trailling slash

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "development" ? false : true,
   },
